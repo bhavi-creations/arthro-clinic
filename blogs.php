589e499dbdd4f0f61dd2d25bb1dcc33b7ab772cb
Ramesh92340 <rameshpilli1428@gmail.com>
2024-10-26 17:51:25 +0530

    tittle  updated

--- blogs.php
+++ blogs.php
@@ -11,7 +11,7 @@ $result = $conn->query($sql);
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Artho</title>
+    <title>Arthro360</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
